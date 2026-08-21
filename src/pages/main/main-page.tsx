f9ecc42087a6fd1b0778df412c65334332037a2e
Wilkin Vásquez <wilkinvasquez@gmail.com>
2026-08-21 15:57:39 -0400

    Add designs

--- src/pages/main/main-page.tsx
+++ src/pages/main/main-page.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import './main-page.css';
 import TranslationRequestService from '../../services/translation-request-service';
 import type { CreateOrUpdateTranslationRequest } from '../../dtos/translation-requests/create-or-update-translation-request';
